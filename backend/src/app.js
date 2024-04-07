6b1167632586af4089c3240ba141dfd9f93a92c7
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-04-07 10:21:59 +0530

    'Add MongoDB connection and export app in separate files'
    This diff includes the following changes:
    
    * In `app.js`, the express app is created and exported.
    * A new file `constants.js` is added, which exports the `DB_NAME` constant.
    * The `index.js` file in the `db` directory is updated to import `mongoose` and `DB_NAME` and defines a function `connectDB` to connect to the MongoDB database using the MONGODB\_URI environment variable and the `DB_NAME`.
    * The `index.js` file in the project root directory is updated to import `dotenv`, `connectDB`, and `app` and sets up the database connection using `connectDB` and starts the server using `app`.

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+
+
+
+const app = express()
+
+
+
+
+
+export { app }
---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.on("errror", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
