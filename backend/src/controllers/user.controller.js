63c73f5bd64a2ba9628d372c1fc22db64a3f9668
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-05-02 01:12:18 +0530

    "Removed 'sameSite' property from cookie options in user.controller.js"
    This commit removes the 'sameSite' property from the cookie options in the 'loginUser' function of the 'user.controller.js'

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -114,7 +114,6 @@ const loginUser = asyncHandler(async (req, res) =>{
    const options = {
     httpOnly: true, 
     secure: true, 
-    sameSite: 'None',
     path: '/', 
     maxAge: 24 * 60 * 60 * 1000 
 };
