bd4818ff6a59332adc90b418ff20aab6a0510913
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-05-02 19:24:14 +0530

    update

--- fortend/src/components/Header/Header.jsx
+++ fortend/src/components/Header/Header.jsx
@@ -56,91 +56,95 @@ function Header({children}) {
 
 
   return (
-    
-    <div className="min-h-[90vh]">
+    <>
+    {isloggedIn && (
+        <div className="min-h-[90vh]">
 
-    <div className="drawer absolute left-0 z-50 w-fit">
-        <input className="drawer-toggle" id="my-drawer" type="checkbox" />
-        <div className="drawer-content">
-            <label htmlFor="my-drawer" className="cursor-pointer relative">
-                <FiMenu 
-                    onClick={changeWidth}
-                    size={"32px"}
-                    className="font-bold text-white m-4"
-                />
-            </label>
-        </div>
-        <div className="drawer-side w-0">
-            <label htmlFor="my-drawer" className="drawer-overlay">
-            </label>
-            <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
-                <li className="w-fit absolute right-2 z-50">
-                <button onClick={hideDrawer}>
-                        <AiFillCloseCircle size={24} />
-                    </button>
-                </li>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-
-                {isloggedIn && role === "admin" && (
-                <li>
-                    <Link to="/admin/dashboard"> Admin DashBoard</Link>
-                </li>
-                )}
-                    
-
-                {isloggedIn && role === 'admin' && (   
+        <div className="drawer absolute left-0 z-50 w-fit">
+            <input className="drawer-toggle" id="my-drawer" type="checkbox" />
+            <div className="drawer-content">
+                <label htmlFor="my-drawer" className="cursor-pointer relative">
+                    <FiMenu 
+                        onClick={changeWidth}
+                        size={"32px"}
+                        className="font-bold text-white m-4"
+                    />
+                </label>
+            </div>
+            <div className="drawer-side w-0">
+                <label htmlFor="my-drawer" className="drawer-overlay">
+                </label>
+                <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
+                    <li className="w-fit absolute right-2 z-50">
+                    <button onClick={hideDrawer}>
+                            <AiFillCloseCircle size={24} />
+                        </button>
+                    </li>
                     <li>
-                        <Link to="/course/create"> Create new course</Link>
+                        <Link to="/">Home</Link>
                     </li>
-                )}
-                <li>
-                    <Link to="/courses">All Courses</Link>
-                </li>
-
-                <li>
-                    <Link to="/contact">Contact Us</Link>
-                </li>
-
-                <li>
-                    <Link to="/about">About Us</Link>
-                </li>
-
-                {!isloggedIn && (
-                    <li className="absolute bottom-4 w-[90%]">
-                        <div className="w-full flex items-center justify-center">
-                            <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
-                                <Link to="/login">Login</Link>
-                            </button>
-                            <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
-                                <Link to="/signup">Signup</Link>
-                            </button>
-                        </div>
+    
+                    {isloggedIn && role === "admin" && (
+                    <li>
+                        <Link to="/admin/dashboard"> Admin DashBoard</Link>
                     </li>
-                )}
-                    
-                {isloggedIn && (
+                    )}
+                        
+    
+                    {isloggedIn && role === 'admin' && (   
+                        <li>
+                            <Link to="/course/create"> Create new course</Link>
+                        </li>
+                    )}
+                    <li>
+                        <Link to="/courses">All Courses</Link>
+                    </li>
+    
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
+    
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+    
+                    {!isloggedIn && (
                         <li className="absolute bottom-4 w-[90%]">
-                        <div className="w-full flex items-center justify-center">
-                            <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
-                                <Link to="/user/profile">Profile</Link>
-                            </button>
-                            <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
-                                <Link onClick={handleLogout} >Logout</Link>
-                            </button>
-                        </div>
-                    </li>
-                )}
-                    
-
-            </ul>
+                            <div className="w-full flex items-center justify-center">
+                                <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link to="/login">Login</Link>
+                                </button>
+                                <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link to="/signup">Signup</Link>
+                                </button>
+                            </div>
+                        </li>
+                    )}
+                        
+                    {isloggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className="w-full flex items-center justify-center">
+                                <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link to="/user/profile">Profile</Link>
+                                </button>
+                                <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link onClick={handleLogout} >Logout</Link>
+                                </button>
+                            </div>
+                        </li>
+                    )}
+                        
+    
+                </ul>
+            </div>
         </div>
+        {children}
+        <hr className="my-8" />
+        <Footer/>
     </div>
-    {children}
-    <hr className="my-8" />
-    <Footer/>
-</div>
+    )}
+    
+    </>
 
 
 
